9209314c27371f72ea851872df6b6cd0e355b55b
SnoopyCodes <97713249+SnoopyCodes@users.noreply.github.com>
2024-05-31 15:36:53 -0500

    wtf strong problem

--- cses/dp/removalgame/removegame.cpp
+++ cses/dp/removalgame/removegame.cpp
@@ -6,8 +6,10 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
     int N; cin >> N;
     vector<int> list(N);
+    ll total = 0;
     for (int i = 0; i < N; i++) {
         cin >> list[i];
+        total += list[i];
     }
     //each player has a score
     //each player at every turn gets 2 choices
@@ -19,8 +21,15 @@ int main() {
     //okay so its basically we can never find a globally optimal substructure 
     //by iterating forward; instead we must go backward
     //
-    ll best = 0;
-    vector<vector<array<ll, 2>>> scores(N + 1, vector<array<ll, 2>>(N+1));
-    //
-    cout << best << "\n";
+    vector<vector<ll>> scores(N, vector<ll>(N));
+    for (int l = N - 1; l >= 0; l--) {
+        for (int r = l; r < N; r++) {
+            if (l == r) { scores[l][r] = list[l]; continue; }
+            //we can select l
+            ll leftScore = list[l] - scores[l+1][r];
+            ll rightScore = list[r] - scores[l][r-1];
+            scores[l][r] = max(leftScore, rightScore);
+        }
+    }
+    cout << (scores[0][N-1] + total) / 2 << "\n";
 }
