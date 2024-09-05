67d01108cd7b7cdcc358349587da80b8a86dbb02
SnoopyCodes <97713249+SnoopyCodes@users.noreply.github.com>
2024-09-05 17:47:26 -0500

    mfw debug with eysbutno's sol

--- cses/glaphs/nrq.cpp
+++ cses/glaphs/nrq.cpp
@@ -1,72 +1,97 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
 vector<int> dsu;
-vector<unordered_set<int>> queries;  //vector
-vector<int> days;
-int find(int v) {
-    if (dsu[v] == v) { return v; }
-    dsu[v] = find(dsu[v]);
-    return dsu[v];
+vector<int> sizes;
+
+vector<vector<array<int, 2>>> mst;
+vector<int> depths;
+
+int find(int u) {
+	if (dsu[u] == u) { return u; }
+	dsu[u] = find(dsu[u]);
+	return dsu[u];
 }
-void unite(int u, int v, int d) {
-    u = find(u);
-    v = find(v);
-    if (u == v) { return; }
-    if (rand() % 2) {
-        dsu[u] = v;
-    }   else {
-        dsu[v] = u;
-        swap(u, v);
-    }
-    //v is parent of u now, move u to v
-    for (int x : queries[u]) {
-        if (queries[v].find(x) == queries[v].end()) {
-            queries[v].insert(x);
-        }   else {
-            days[x] = d;
-            queries[v].erase(x);
-        }
-    }
+void merge(int u, int v, int d) {
+	mst[u].push_back({v, d});
+	mst[v].push_back({u, d});
+	u = find(u);
+	v = find(v);
+	if (sizes[u] < sizes[v]) { swap(u, v); }  //merge v to u
+	sizes[u] += sizes[v];
+	dsu[v] = u;
 }
+vector<vector<int>> jumps;
+vector<vector<int>> vals;
+vector<bool> visited;
+void dfs(int u, int from, int d, int val) {
+	depths[u] = d;
+	visited[u] = true;
+	jumps[0][u] = from;
+    vals[0][u] = val;
+	for (auto const&[v, dist] : mst[u]) {
+		if (v == from) { continue; }
+		dfs(v, u, d + 1, dist);
+	}
+}
+
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr);
-    //we may utilize dsu on this for sure
-    //but are we really to add queries to each cc
-    //and then check each cc?
-    //we can instead mark for each querie which cc it belongs to, then binary search on the first day 
-    //for which they are the same?
-    //naive approach is a scary O(MQ alpha(M)) where we check every day every query
-    //storing for node where it goes...
-    //this is the same as dragging it along isnt it?
-    //honestly i bet this passes anyways like it should not but it probably will
-    //actually i dont believe it
-    //lmao
-    //dsu is simply too weak
-    //ok so euler tour
-    //for every dsu we need to build
-    int N, M, Q; cin >> N >> M >> Q;
-    vector<array<int, 2>> edges(M);
-    queries.resize(N);
-    dsu.resize(N);
-    days.resize(Q, -1);
-    for (int i = 0; i < M; i++) {
-        int a, b; cin >> a >> b; a--; b--;
-        edges[i] = {a, b};
-    }
-    for (int i = 0; i < N; i++) {
-        dsu[i] = i;
-    }
-    for (int i = 0; i < Q; i++) {
-        int a, b; cin >> a >> b; a--; b--;
-        queries[a].insert(i);
-        queries[b].insert(i);
-        //figure out how to add it
-    }
-    for (int i = 0; i < M; i++) {
-        unite(edges[i][0], edges[i][1], i+1);
-    }
-    for (int i = 0; i < 5; i++) {
-        cout << days[i] << "\n";
+	cin.tie(0) -> sync_with_stdio(0);
+	int N, M, Q; cin >> N >> M >> Q;
+	dsu.resize(N);
+	mst.resize(N);
+	depths.resize(N);
+	for (int i = 0; i < N; i++) { dsu[i] = i; }
+	sizes.resize(N, 1);
+	for (int i = 0; i < M; i++) {
+		int a, b; cin >> a >> b; a--; b--;
+		if (find(a) != find(b)) { merge(a, b, i + 1); }
+	}
+	
+	visited.resize(N);
+	int max_jumps = __lg(N - 1) + 1;
+	jumps.resize(max_jumps, vector<int>(N));
+	vals.resize(max_jumps, vector<int>(N));
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]) { dfs(i, -1, 0, 0); }
+	}
+	for (int i = 1; i < max_jumps; i++) {
+		for (int j = 0; j < N; j++) {
+			if (jumps[i-1][j] != -1) {
+				jumps[i][j] = jumps[i-1][jumps[i-1][j]];
+				vals[i][j] = max(vals[i-1][j], vals[i-1][jumps[i-1][j]]);
+			}
+		}
+	}
+	
+    for (int q = 0; q < Q; q++) {
+        int a, b; cin >> a >> b; a--;  b--;
+		if (find(a) != find(b)) { cout << -1 << "\n"; continue; }
+        //now we need to find like
+		int ma = 0, mb = 0;
+        if (depths[b] > depths[a]) { swap(a, b); }
+        int equalize = depths[a] - depths[b];
+        for (int i = 0; i < max_jumps; i++) {
+            if (equalize & 1 << i) {
+				ma = max(ma, vals[i][a]);
+				a = jumps[i][a];
+			}
+        }
+        for (int i = max_jumps - 1; i > -1; i--) {
+            if (jumps[i][a] != jumps[i][b]) {
+				ma = max(ma, vals[i][a]);
+                a = jumps[i][a];
+				mb = max(mb, vals[i][b]);
+                b = jumps[i][b];
+            }
+        }
+		if (a != b) {
+			ma = max(ma, vals[0][a]);
+			mb = max(mb, vals[0][b]);
+		}
+		cout << max(ma, mb) << "\n";
+        //lca obtained, in theory
+		//what about 3 5 screws it up?
+		//ok why did our lca just die
     }
 }
--- cses/glaphs/pqii.cpp
+++ cses/glaphs/pqii.cpp
@@ -1,14 +1,97 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
+//i am in pain
+vector<int> tp;
+vector<int> dists; // dist from cycle if not cycle, cycle idx if in
+vector<int> in_cycle;
+vector<vector<int>> cycles;
+stack<int> path;
+vector<int> visited;
+const int done = 2;
+const int stacked = 1;
+ 
+int ncycles = 0;
+int dfs(int u) {
+	visited[u]++;
+	if (visited[tp[u]] == done) {  //we have visited this before
+		if (in_cycle[tp[u]] != -1) { dists[u] = 1; } //in cycle
+		else { dists[u] = dists[tp[u]] + 1; }  //leads to cycle
+		visited[u] = 2;
+		return dists[u] + 1;
+	}	else if (visited[tp[u]] == stacked) {
+		//create cycle..
+		cycles.push_back({});
+		int ep = tp[u]; //endpoint
+		path.push(u);
+		int popped = -1;
+		int idx = 0;
+		while (popped != ep) {
+			//wtf
+			popped = path.top();
+			path.pop();
+			in_cycle[popped] = ncycles;
+			cycles[ncycles].push_back(popped);
+			visited[popped] = 2;
+			dists[popped] = idx++;
+		}
+		ncycles++;
+		return 1;
+	}	else {
+		path.push(u);
+		int res = dfs(tp[u]);
+		visited[u] = 2;
+		//if we are not in cycle, set everything
+		if (in_cycle[u] == -1) { path.pop(); dists[u] = res; return res+1; }
+		return res;
+	}
+}
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
-    int N, Q; cin >> N >> Q;
-    int max_jumps = __lg(N - 1) + 1;
-    vector<vector<int>> jumps(max_jumps, vector<int>(N));
-    for (int i = 0; i < N; i++) {
-        
-    }
-    //
+	int N, Q; cin >> N >> Q;
+	int max_jumps = __lg(N - 1) + 1;
+	vector<vector<int>> jumps(max_jumps, vector<int>(N));
+	dists.resize(N, -1);
+	tp.resize(N);
+	visited.resize(N);
+	in_cycle.resize(N, -1);
+	
+	for (int i = 0; i < N; i++) {
+		int k; cin >> k; k--;
+		jumps[0][i] = k;
+		tp[i] = k;
+	}
+	for (int i = 1; i < max_jumps; i++) { //this is fine
+		for (int j = 0; j < N; j++) {
+			jumps[i][j] = jumps[i-1][jumps[i-1][j]];
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		if (!visited[i]) { dfs(i); }
+	}
+	for (int q = 0; q < Q; q++) {
+		int a, b; cin >> a >> b; a--; b--;
+		//ok now we need to take in queries...
+		int disttocycle = 0;
+		if (in_cycle[b] != -1 && in_cycle[a] == -1) { //try to lift a up to cycle
+			disttocycle = dists[a];
+			for (int i = 0; i < max_jumps; i++) {
+				if (disttocycle & 1 << i) { a = jumps[i][a]; }
+			}
+		}
+		if (in_cycle[a] == in_cycle[b] && in_cycle[a] != -1) {  //in same cycle
+			cout << disttocycle + (dists[a] - dists[b] + cycles[in_cycle[a]].size())
+				% cycles[in_cycle[a]].size() << "\n";
+		} 	else if (in_cycle[a] == in_cycle[b]){ //both in tree i guess
+			int diff = dists[a] - dists[b];
+			if (diff < 0) { cout << -1 << "\n"; continue; }
+			for (int i = 0; i < max_jumps; i++) {
+				if (diff & 1 << i) { a = jumps[i][a]; }
+			}
+			if (a != b) { cout << -1 << "\n"; continue; }
+			cout << diff << "\n";
+		}	else {
+			cout << -1 <<"\n";
+		}
+		
+	}
 }
