73244fa68c273716df09d7c50a4ae7646aa51e07
SnoopyCodes <97713249+SnoopyCodes@users.noreply.github.com>
2024-10-10 11:19:22 -0500

    reorganize

--- gold/dp/maxindset.cpp
+++ gold/dp/maxindset.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+
 using ll = long long;
-ll bits(ll x) {
-    return __builtin_popcountll(x);
-}
-signed main() {
+ll bits(ll x) { return __builtin_popcountll(x); }
+int main() {
     cin.tie(0) -> sync_with_stdio(0);
     int N, M; cin >> N >> M;
     vector<ll> reach(N);
@@ -20,43 +18,36 @@ signed main() {
     vector<ll> right;
     int r = N - N / 2;
     int l = N / 2;
-    for (int mask = 0; mask < 1LL << r; mask++) {
+    for (int mask = 0; mask < 1 << r; mask++) {
         ll options = 0;
         for (int b = 0; b < r; b++) {
-            if (mask & 1LL << b) {  //we can reach all that this reaches
-                options |= reach[b + l];
-            }
+            if (mask & 1 << b) { options |= reach[b + l]; }
         }
         bool valid = true;
         for (int b = 0; b < r; b++) {
-            if (mask & 1LL << b && options & 1LL << b + l) { valid = false; break; }
+            if (mask & 1 << b && options & 1LL << b + l) { valid = false; break; }
         }
         if (valid) { right.push_back((ll)mask << l); }
     }
 
-    vector<ll> dp(1LL << l);
-    vector<ll> res(1LL << l);
+    vector<ll> dp(1 << l);
+    vector<ll> res(1 << l);
     for (ll rset : right) {
         //this is a recalculation
         ll options = 0;
         for (int b = 0; b < N; b++) {
-            if (rset & 1LL << b) {  //we can reach all that this reaches
-                options |= reach[b];
-            }
+            if (rset & 1LL << b) { options |= reach[b]; }
         }
         for (int b = l; b < N; b++) {
             if (options & 1LL << b) { options ^= 1LL << b; }
         }
-        ll mask = ((1LL << l) - 1) ^ options;
-    // Update 'res' only if the new rset has more nodes
-    if (bits(res[mask]) < bits(rset)) {
-        res[mask] = rset;
-    }
+        ll invert = ((1LL << l) - 1) ^ options;  //idk what happens if we xor an int with a ll
+        if (bits(res[invert]) < bits(rset)) { res[invert] = rset; }
     }
 
     int ans = 0;
     ll ans_set = 0;
-    for (int mask = 0; mask < 1LL << l; mask++) {
+    for (int mask = 0; mask < 1 << l; mask++) {
         if (ans < bits(dp[mask]) + bits(res[mask])) {
             ans = bits(dp[mask]) + bits(res[mask]);
             ans_set = dp[mask] | res[mask];
@@ -64,14 +55,14 @@ signed main() {
 
         ll options = 0;
         for (int b = 0; b < l; b++) {
-            if (mask & 1LL << b) {  //we can reach all that this reaches
+            if (mask & 1 << b) {  //we can reach all that this reaches
                 options |= reach[b];
             }
         }
 
         bool valid = true;
         for (int b = 0; b < l; b++) {
-            if (mask & 1LL << b && options & 1LL << b) { valid = false; break; }
+            if (mask & 1 << b && options & 1 << b) { valid = false; break; }
         }
         if (valid) {
             if (bits(dp[mask]) < bits(mask)) {
@@ -83,8 +74,8 @@ signed main() {
             }
         }
         for (int b = 0; b < l; b++) {
-            if (bits(dp[mask | 1LL << b]) < bits(dp[mask])) {
-                dp[mask | 1LL << b] = dp[mask];
+            if (bits(dp[mask | 1 << b]) < bits(dp[mask])) {
+                dp[mask | 1 << b] = dp[mask];
             }
         }
     }
