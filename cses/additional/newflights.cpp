7376f0c6eb90c0b0d9817a11e2386bec46ad10e8
SnoopyCodes <97713249+SnoopyCodes@users.noreply.github.com>
2024-11-02 16:32:00 -0500

    more trivialer than i thought

---
+++ cses/additional/newflights.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    //find sccs, and figure out which ones can reach which
+    //by sccs, we reduce it to: for a DAG, find min edges to add
+    //so all nodes can reach all others
+    //this is very hard
+    //dp[i] = min to connect all components of 
+}
--- gold/topo/patternreal.cpp
+++ gold/topo/patternreal.cpp
@@ -1,16 +1,15 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
-    //hopefully is fun
-    //this looks really fun actually i like it
-
-    //misread, goddamn it
-    //what this does is it places stuff in an order such that for each constraint
-    //the first string it matches is position i
+    //trivially connect stuff, bruh
+    //here is what i misread as (and a slightly more confusing problem)
+    //for each constraint string s, int j, find an ordering
+    //such that string j in the final ordering is the first one that it matches
     int N, M, K; cin >> N >> M >> K;
-    vector<string> P(N);  //patterns- nodes
+    vector<string> P(N);  //patterns- nodes. you only need the map, but i still have strings here
     vector<int> in(N);
     map<string, int> m;
     vector<vector<int>> constraints(N);  //edges from indexes (here) to nodes
@@ -21,41 +20,42 @@ int main() {
     for (int i = 0; i < M; i++) {
         string s; cin >> s;
         int j; cin >> j; j--;
-        //edge to all that match this one
+        for (int k = 0; k < K; k++) {
+            if (s[k] != P[j][k] && P[j][k] != '_') { cout << "NO" << "\n"; return 0; }
+        }
         for (int mask = 0; mask < 1 << K; mask++) {
             string match = s;
             for (int b = 0; b < 1 << K; b++) {
                 if (mask & 1 << b) { match[b] = '_'; }
             }
             if (m.count(match)) {
+                if (match == P[j]) { continue; }
                 int idx = m[match];
-                cout << idx << " " << j << "\n";
                 constraints[j].push_back(idx);
                 in[idx]++;
             }
         }
     }
-    vector<int> order;
     queue<int> q;
+    vector<int> order;
     for (int i = 0; i < N; i++) {
         if (in[i] == 0) { q.push(i); }
-        cout << in[i] << " ";
     }
-    cout << "\n";
-    for (int i = 0; i < N; i++) {
-        for (int x : constraints[i]) {
-            in[x]--;
-            if (in[x] == 0) { q.push(x); }
-        }
-        if (q.empty()) { cout << "NO" << "\n"; return 0; }
+    while (!q.empty()) {
         int u = q.front();
-        cout << u << "\n";
         q.pop();
         order.push_back(u);
+        for (int v : constraints[u]) {
+            in[v]--;
+            if (in[v] == 0) { q.push(v); }
+        }
     }
-    cout << "YES" << "\n";
-    for (int i = 0; i < N; i++) {
-        cout << order[i] + 1 << " \n"[i == N - 1];
+    if (order.size() == N) {
+        cout << "YES" << "\n";
+        for (int i = 0; i < N; i++) {
+            cout << order[i] + 1 << " \n"[i == N - 1];
+        }
+    }   else {
+        cout << "NO" << "\n";
     }
-
 }
