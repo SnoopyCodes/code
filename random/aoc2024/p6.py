ce2551729faff14e1ca710a329319dd9a0f850e5
SnoopyCodes <97713249+SnoopyCodes@users.noreply.github.com>
2024-12-04 17:02:36 -0600

    aocapparently

---
+++ random/aoc2024/p6.py
@@ -0,0 +1,22 @@
+import os
+os.chdir("C:\\Users\\benja\\VSC\\code\\random\\aoc2024")
+with open("in.txt", "r") as file:
+    lines = file.readlines()
+sum = 0
+on = True
+for mem in lines:
+    for i in range(len(mem)):
+        if mem[i:i+4] == "mul(" and on:
+            comma = mem[i:].find(",") + i
+            end = mem[i:].find(")") + i
+            if comma == -1 or end == -1:
+                continue
+            num1 = mem[i+4:comma]
+            num2 = mem[comma+1:end]
+            if num1.isdecimal() and num2.isdecimal() and len(num1) < 4 and len(num2) < 4:
+                sum += int(num1) * int(num2)
+        elif mem[i:i+7] == "don't()":
+            on = False
+        elif mem[i:i+4] == "do()":
+            on = True
+print(sum)
