321c54b28b0b2f8014d9bebacea6d79d9f210db0
SnoopyCodes <97713249+SnoopyCodes@users.noreply.github.com>
2025-02-20 16:48:33 -0600

    slightly better template

--- template/TEMPLATE.cpp
+++ template/TEMPLATE.cpp
@@ -5,11 +5,10 @@
 #define arr array
 #define GET_MACRO(_1, _2, _3, NAME, ...) NAME
 #define rep(...) GET_MACRO(__VA_ARGS__, rep3, rep2)(__VA_ARGS__)
-#define rep3(x,s,e) for(auto x=(s);x!=(e);(s)<(e)?x++:x--)
-#define rep2(x,e) rep3(x,(e)>0?0:-(e)-1,(e)>0?(e):-1)
+#define rep3(x,s,e) for(auto x=s;x!=e;s<e?x++:x--)
+#define rep2(x,e) rep3(x,(e>0?0:-(e)-1),(e>0?e:-1))
 
-const int INF = 1e9;
-const long INFL = 4e18;
+const long INF = 4e18 + 7e9;
 
 using namespace std;
 
--- template/testtemplate.cpp
+++ template/testtemplate.cpp
@@ -5,14 +5,13 @@
 #define arr array
 #define GET_MACRO(_1, _2, _3, NAME, ...) NAME
 #define rep(...) GET_MACRO(__VA_ARGS__, rep3, rep2)(__VA_ARGS__)
-#define rep3(x,s,e) for(auto x=s;x!=e;s<e?x++:x--)
-#define rep2(x,e) rep3(x,(e>0?0:-(e)-1),(e>0?e:-1))
+#define rep3(x,s,e) for(auto x = (s); x != (e); (s) < (e) ? x++ : x--)
+#define rep2(x,e) rep3(x, (e) > 0 ? 0 : -(e)-1, (e) > 0 ? (e) : -1)
 
-const long INF = (1LL << 62) - (1LL << 32) + (int(1e9) + 10);
+const long INF = (1LL << 62) - (1LL << 32) + int(1e9 + 5);
 
 using namespace std;
 
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
-    
 }
